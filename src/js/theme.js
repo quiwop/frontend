1403d4beec7a058bc3a1d5ba6db99f6193960d42
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-10-23 18:03:00 -0500

    Button to activate deactivate the dark mode.

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,4 +1,39 @@
 const body = document.body;
+export function changeTheme(btn) {
+  const $btn = document.getElementById(btn);
+  const $icon = document.querySelector(".mode .mode-text i");
+  const $text = document.querySelector(".mode .mode-text p");
+  if (localStorage.getItem("theme") === "dark-theme") {
+    body.classList.add("dark-theme");
+    $btn.classList.add("active");
+    $icon.classList.add("fa-moon");
+    $icon.classList.remove("fa-sun");
+    $text.textContent = "Modo oscuro";
+  } else {
+    body.classList.remove("dark-theme");
+    $btn.classList.remove("active");
+    $icon.classList.remove("fa-moon");
+    $icon.classList.add("fa-sun");
+    $text.textContent = "Modo claro";
+  }
+  $btn.addEventListener("click", () => {
+    if (body.classList.contains("dark-theme")) {
+      body.classList.remove("dark-theme");
+      $btn.classList.remove("active");
+      $icon.classList.remove("fa-moon");
+      $icon.classList.add("fa-sun");
+      $text.textContent = "Modo claro";
+      localStorage.setItem("theme", "light-theme");
+    } else {
+      body.classList.add("dark-theme");
+      $btn.classList.add("active");
+      $icon.classList.add("fa-moon");
+      $icon.classList.remove("fa-sun");
+      $text.textContent = "Modo oscuro";
+      localStorage.setItem("theme", "dark-theme");
+    }
+  });
+}
 
 export function theme() {
   if (!localStorage.getItem("theme")) {
