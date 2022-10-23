9eac23beb217be736bdc3e1e4374803036cfe122
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-10-23 15:07:20 -0500

    Global HTML, for all pages.

---
+++ src/js/globalHtmlHead.js
@@ -0,0 +1,15 @@
+export default function htmlHead() {
+  document.querySelector("head").insertAdjacentHTML(
+    "beforeend",
+    `
+      <!-- Global styles -->
+      <link rel="stylesheet" href="./src/css/global.css" />
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="https://quiwop.github.io/media/favicon.png" />
+      <!-- Font Family -->
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+    `
+  );
+}
