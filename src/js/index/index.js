930f1831995a9316f37558c98085c4722f90d2dd
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-10-23 15:19:18 -0500

    Integrating menu.

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,9 +1,13 @@
 import htmlHead from "./htmlHead_1.js";
 import header from "./header_1.js";
+import menu from "../menu.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   htmlHead();
   if (document.querySelector("header")) {
     header("header");
+    if (document.querySelector("header").classList.contains("header")) {
+      menu("icon-menu", "container-menu");
+    }
   }
 });
